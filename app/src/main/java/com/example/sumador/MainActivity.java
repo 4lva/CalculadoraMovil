a22b0d66517cc46fa038460c6e57dc458cca0e82
4lva <alvarosazamora@gmail.com>
2022-09-29 11:20:40 +0200

    Mejora de interfaz y deteccion de falta de numeros

--- app/src/main/java/com/example/sumador/MainActivity.java
+++ app/src/main/java/com/example/sumador/MainActivity.java
@@ -9,30 +9,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et1;
-    EditText et2;
-    TextView tv1;
+    EditText num1;
+    EditText num2;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1=findViewById(R.id.editTextNumber);
-        et2=findViewById(R.id.editTextNumber2);
-        tv1=findViewById(R.id.textView);
+        num1=findViewById(R.id.numero1);
+        num2=findViewById(R.id.numero2);
+        result=findViewById(R.id.resultado);
     }
 
-    public void sumar(View view) {
+    public void calcular(View view) {
+        //comprobamos si se ha metido algun numero para poder calcular
+        if(num1.getText().toString().isEmpty()&&num2.getText().toString().isEmpty()){
+            result.setText("No has introducido numeros");
+        }else {
+
+        }
 
         // Obtenemos los números
-        int n1=Integer.parseInt(et1.getText().toString());
-        int n2=Integer.parseInt(et2.getText().toString());
+        //int n1=Integer.parseInt(num1.getText().toString());
+        //int n2=Integer.parseInt(num2.getText().toString());
 
         // Hacemos la suma
-        int suma=n1+n2;
+        //int suma=n1+n2;
 
         // Establecemos el resultado en el TextView
-        tv1.setText(""+suma);
+        //result.setText(""+suma);
     }
 }
